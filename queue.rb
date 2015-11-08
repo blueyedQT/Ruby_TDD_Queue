f974c3bfdcd32c747a98814e6fbaeddeab467a41
Katrina <blueyed_qt@hotmail.com>
2015-11-08 10:22:32 -0800

    Set up for Queue

---
+++ queue.rb
@@ -0,0 +1,3 @@
+class Queue
+	 # additionally, you will need to create a 'enque_display' function that will simply return the @data_store variable for the tests to pass, because one of the test cases utilizes this function to validate the @data_store array.
+end
